43b3f1d6f4b4d06487a1c7a1d7a309e0f229e48a
MarcoMLY <96983402+MarcoMLY@users.noreply.github.com>
2025-08-27 10:38:46 +0100

    Added sprites

--- snake.gd
+++ snake.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 @export var movementCooldownHold = 200
-@export var movementCooldownTap = 10
+@export var movementCooldownTap = 5
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
